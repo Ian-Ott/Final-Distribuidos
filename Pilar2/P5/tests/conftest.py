5ca2f4bc04d5b92335d67bdbdb46498771558e0f
ianott <ianlucasott@gmail.com>
2026-08-07 03:26:33 +0000

    fix path

--- Pilar2/P5/tests/conftest.py
+++ Pilar2/P5/tests/conftest.py
@@ -1,10 +1,17 @@
+import os
+import sys
+
 import pytest
 import pika
 
+BASE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")
+)
+
+sys.path.insert(0, BASE)
 
 @pytest.fixture
 def rabbitmq_channel():
-
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
             host="localhost",
